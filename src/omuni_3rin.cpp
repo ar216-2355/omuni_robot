53984a4401755f96beb874fcc0931a1fb6ed8b32
ar216-2355 <ar216gfmem2355@gmail.com>
2026-01-05 21:32:48 +0900

    delete remote

--- src/omuni_3rin.cpp
+++ src/omuni_3rin.cpp
@@ -4,6 +4,48 @@
 #include "robomas_package_2/msg/motor_cmd.hpp"
 #include "robomas_package_2/msg/ems.hpp"
 
+/*
+
+     +
+     ^
+     |
++ <- o -> -
+     |  
+     v
+     -
+
+y軸はいつも通り、x軸は左右反転
+*/
+
+// ボタン
+#define A 0
+#define B 1
+#define X 2
+#define Y 3
+#define LB 4
+#define RB 5
+#define BACK 6
+#define START 7
+#define home_btn 8
+#define L_stick_btn 9
+#define R_stick_btn 10
+
+// 軸
+#define L_stick_x 0
+#define L_stick_y 1
+#define R_stick_x 2
+#define R_stick_y 3
+#define LT 4   // 押してない状態で 1 -> 最後まで押して -1
+#define RT 5   // 押してない状態で 1 -> 最後まで押して -1
+#define D_pad_x 6
+#define D_pad_y 7
+
+// 制御モード番号
+#define CURRENT 0
+#define VELOCITY 1
+#define POSITION 2 
+
+
 class Omuni3Rin : public rclcpp::Node
 {
 public:
@@ -17,6 +59,11 @@ public:
 
 private:
     void joy_callback(const sensor_msgs::msg::Joy::SharedPtr msg){
+        handle_ems(msg);
+        publish_motor_cmd(msg);
+    }
+
+    void handle_ems(const sensor_msgs::msg::Joy::SharedPtr msg){
         if(msg->buttons[6]){
             // 非常停止モードへ
             robomas_package_2::msg::Ems EMS;
@@ -31,40 +78,51 @@ private:
             pub_ems_1_->publish(EMS);
             pub_ems_2_->publish(EMS);
         }
+    }
+
+    void publish_motor_cmd(const sensor_msgs::msg::Joy::SharedPtr msg){
+        robomas_package_2::msg::MotorCmdArray can0;
+        robomas_package_2::msg::MotorCmdArray can1;
+        robomas_package_2::msg::MotorCmd can0_motor;
+        robomas_package_2::msg::MotorCmd can1_motor;
+
+        can0_motor.id = 1;
+        can0_motor.mode = VELOCITY;
+        can0_motor.value = msg->axes[L_stick_y] * 1000.0;
+        can0.cmds.push_back(can0_motor);
         
-        robomas_package_2::msg::MotorCmdArray out1;
-        robomas_package_2::msg::MotorCmd cmd1;
+        can0_motor.id = 2;
+        can0_motor.mode = VELOCITY;
+        can0_motor.value = msg->axes[L_stick_x] * -1000.0;
+        can0.cmds.push_back(can0_motor);
 
-        // --- motor1: 右スティックX（axes[3]） ---
-        cmd1.id = 1;
-        cmd1.mode = 1;  // SPEED mode
-        cmd1.value = msg->axes[3] * 1000.0f;
-        out1.cmds.push_back(cmd1);
-
-        // --- motor2: 左スティックX（axes[0]） ---
-        cmd1.id = 2;
-        cmd1.mode = 1;
-        cmd1.value = msg->axes[0] * 1000.0f;
-        out1.cmds.push_back(cmd1);
-
-        // --- motor3: LT (axes[2]) ---
-        // ※コントローラによって値が 1→-1 or 0→1 など違うため必要に応じて調整
-        cmd1.id = 3;
-        cmd1.mode = 1;
-        cmd1.value = msg->axes[2] * 1000.0f;
-        out1.cmds.push_back(cmd1);
-
-        pub_motor_1_->publish(out1);
+        can0_motor.id = 3;
+        can0_motor.mode = POSITION;
+        if(msg->buttons[A]) can0_motor.value = 100.0;
+        else if(msg->buttons[B]) can0_motor.value = -100.0;
+        else can0_motor.value = 0.0;
+        can0.cmds.push_back(can0_motor);
 
         
+        can1_motor.id = 1;
+        can1_motor.mode = VELOCITY;
+        can1_motor.value = msg->axes[R_stick_y] * 1000.0;
+        can1.cmds.push_back(can1_motor);
 
-        RCLCPP_INFO(
-            this->get_logger(),
-            "m1=%.1f  m2=%.1f  m3=%.1f",
-            out1.cmds[0].value,
-            out1.cmds[1].value,
-            out1.cmds[2].value
-        );
+        can1_motor.id = 2;
+        can1_motor.mode = VELOCITY;
+        can1_motor.value = msg->axes[R_stick_x] * -1000.0;
+        can1.cmds.push_back(can1_motor);
+
+        can1_motor.id = 3;
+        can1_motor.mode = POSITION;
+        if(msg->buttons[A]) can1_motor.value = 100.0;
+        else if(msg->buttons[B]) can1_motor.value = -100.0;
+        else can1_motor.value = 0.0;
+        can0.cmds.push_back(can1_motor);
+
+        pub_motor_1_->publish(can0);
+        pub_motor_2_->publish(can1);
     }
 
     rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr sub_joy_;
